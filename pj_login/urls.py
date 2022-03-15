77080b87880cf4e27086f766def6202b7b76188d
Shibata <suguru.shibata@blowish.jp>
2022-03-15 17:14:35 +0900

    initial commit

---
+++ pj_login/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import TemplateView
+from registration import views
+
+# 実はページを表示するだけならこのように1行で書くことが出来ます。
+index_view = TemplateView.as_view(template_name="registration/index.html")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", login_required(index_view), name="index"),
+    path('', include("django.contrib.auth.urls")),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
+    path('activate/<uidb64>/<token>/', views.ActivateView.as_view(), name='activate'),
+]
